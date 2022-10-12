080531ed75949d7ac12df476f89cdbf848111edf
Aryaman <someoneg4@gmail.com>
2022-10-12 19:13:13 +0530

    base skeleton

--- shell.c
+++ shell.c
@@ -1,3 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
+int main() {
+    put("Welcome to Aryaman's shell!\n");
+    shell();
+    return 0;
+}
+
+void shell() {
+    // cd echo pwd
+    // ls cat date rm mkdir
+    put("Enter command: ");
+    
+    char userInp[512]; //will store user input
+    fgets(userInp, sizeof(userInp), stdin);
+
+    char *tokenInput = strtok(userInp, "");
+
+    if (strcmp(tokenInput, "cd")) {
+        put("Changing directory...\n");
+        cd(tokenInput);
+    }
+}
+
+void cd(char* cmd) {
+    put("Hey");
+    return(0);
+}
