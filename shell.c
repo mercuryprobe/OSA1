05fdce47befc33192f1e53df9460180aa166b69a
Aryaman <someoneg4@gmail.com>
2022-10-13 00:30:23 +0530

    more debug

--- shell.c
+++ shell.c
@@ -25,10 +25,10 @@ void shell() {
     if (strcmp(tokenInput, "cd")==0) {
         // reminder: handle "cd\n" case
         char[256] buf;    
-        char *getcwd(char *buf, sizeof(buf));
+        getcwd(char *buf, sizeof(buf);
         printf("%s\n", buf);
         printf("Changing directory...\n");
-        char *getcwd(char *buf, sizeof(buf));
+        getcwd(char *buf, sizeof(buf);
         printf("%s\n", buf);
 
         // cd(tokenInput);
