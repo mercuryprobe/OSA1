624890af558b8f28e7a1ea9fddb10c52a0f53891
Aryaman <someoneg4@gmail.com>
2022-10-13 01:19:49 +0530

    try

--- shell.c
+++ shell.c
@@ -4,7 +4,14 @@
 #include <unistd.h>
 
 void cd(char* cmd) {
-    
+    char buf[256];    
+    getcwd(buf, sizeof(buf));
+    printf("%s\n", buf);
+    printf("Changing directory...\n");
+    chdir(cmd);
+    getcwd(buf, sizeof(buf));
+    printf("%s\n", buf);
+
     printf("ok");
 }
 
@@ -12,26 +19,26 @@ void cd(char* cmd) {
 void shell() {
     // cd echo pwd
     // ls cat date rm mkdir
-    printf("Enter command: ");
+    printf("rmnShell>");
     
     char userInp[512]; //will store user input
     fgets(userInp, sizeof(userInp), stdin);
 
     char *tokenInput = strtok(userInp, " ");
-    // printf("%d", strcmp(tokenInput, "cd\n"));
-    // printf("%s",tokenInput);
-
+    char *flagComputeText = *tokenInput;
+    flagComputeText[0] = "no";
+    puts(tokenInput);
+    puts(flagComputeText);
     
-    if (strcmp(tokenInput, "cd")==0) {
-        // reminder: handle "cd\n" case
-        char buf[256];    
-        getcwd(buf, sizeof(buf));
-        printf("%s\n", buf);
-        printf("Changing directory...\n");
-        chdir("..");
-        getcwd(buf, sizeof(buf));
-        printf("%s\n", buf);
+    if (strcmp(tokenInput, "exit\n")==0) {
+        //exit
+        return;
+    }
 
+    if (strcmp(tokenInput, "cd")==0) {
+        //
+        // reminder: handle "cd\n" case
+        
         // cd(tokenInput);
         
     }
@@ -40,6 +47,8 @@ void shell() {
 
 int main() {
     puts("Welcome to Aryaman's shell!");
-    shell();
+    while true{
+        shell();
+        }
     return 0;
 }
