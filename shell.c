1b15ab69e7ffe6ce499d0e38512484e571cf4172
Aryaman <someoneg4@gmail.com>
2022-10-13 01:41:10 +0530

    ok

--- shell.c
+++ shell.c
@@ -19,52 +19,52 @@ void cd(char* cmd) {
 void shell() {
     // cd echo pwd
     // ls cat date rm mkdir
-    printf("rmnShell>");
-    
-    char userInp[512]; //will store user input
-    fgets(userInp, sizeof(userInp), stdin);
+    while (1)
+    {    printf("rmnShell>");
+        
+        char userInp[512]; //will store user input
+        fgets(userInp, sizeof(userInp), stdin);
 
-    char *tokenInput = strtok(userInp, " ");
-    char *flagComputeText = strtok(userInp, " ");
-    
-    char flag1[64];
-    char flag2[64];
+        char *tokenInput = strtok(userInp, " ");
+        char *flagComputeText = strtok(userInp, " ");
+        
+        char flag1[64];
+        char flag2[64];
 
-    int flag1Taken = 0;
-    while (flagComputeText != NULL)
-    {
-        if (flagComputeText[0]=='-'){
-            if (flag1Taken == 0){
-                strcpy(flag1, flagComputeText);
-            } else {
-                strcpy(flag2, flagComputeText);
+        int flag1Taken = 0;
+        while (flagComputeText != NULL)
+        {
+            if (flagComputeText[0]=='-'){
+                if (flag1Taken == 0){
+                    strcpy(flag1, flagComputeText);
+                } else {
+                    strcpy(flag2, flagComputeText);
+                }
             }
+            flagComputeText = strtok(NULL, " ");
         }
-        flagComputeText = strtok(NULL, " ");
-    }
-    // puts(flag1);
-    // puts(flag2);
-    // puts(flagComputeText);
+        // puts(flag1);
+        // puts(flag2);
+        // puts(flagComputeText);
 
-    if (strcmp(tokenInput, "exit\n")==0) {
-        //exit
-        break;
-    }
+        if (strcmp(tokenInput, "exit\n")==0) {
+            //exit
+            return;
+        }
 
-    if (strcmp(tokenInput, "cd")==0) {
-        //
-        // reminder: handle "cd\n" case
-        
-        // cd(tokenInput);
-        
+        if (strcmp(tokenInput, "cd")==0) {
+            //
+            // reminder: handle "cd\n" case
+            
+            // cd(tokenInput);
+            
+        }
     }
 }
 
 
 int main() {
     puts("Welcome to Aryaman's shell!");
-    while (1){
-        shell();
-        }
+    shell();
     return 0;
 }
