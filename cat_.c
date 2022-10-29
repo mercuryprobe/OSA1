950a9ce7aa2cce2cd558a05cdb2e9bc8bf76921e
Aryaman <someoneg4@gmail.com>
2022-10-29 23:42:48 +0530

    try

--- cat_.c
+++ cat_.c
@@ -88,7 +88,7 @@ void cat(char cmd[512][512], int flag1, int flag2, int posn, int last) {
             //edge case: no filename input after cat >
             printf("Error: No filename entered.\n");
             return;
-        }
+        } 
     }
     
     fclose(file);
@@ -104,6 +104,7 @@ int main(int argc, char *argv[]) {
     char splitString[512][512];
     for (int i = 1; i<argc; i++) {
         strcpy(splitString[i-1], argv[i]);
+        printf(splitString[i-1]);
     }
     printf("%d\n", argc);
     splitString[0][strcspn(splitString[0], "\n")]=0;
@@ -113,7 +114,7 @@ int main(int argc, char *argv[]) {
     int flag2 = floogs.flag2;
     int flag1Taken = floogs.flag1Taken;
     int flag2Taken = floogs.flag2Taken;
-
+    printf("%d %d %d %d\n", flags1, flag2, flag1Taken, flag2Taken);
     //run function
     puts("3");
     cat(splitString, flag1, flag2, 1 + flag1Taken + flag2Taken, argc+1);
