d5af925d373291fcb2c923195f742041e77d298a
Aryaman <someoneg4@gmail.com>
2022-10-31 03:47:30 +0530

    f

--- shell.c
+++ shell.c
@@ -162,7 +162,7 @@ void echo(char cmd[512][512], int flag1, int flag2, int posn, int last) {
 static volatile sig_atomic_t interrupted = 0;
 static void interrupter(int x) {
     //reference: https://stackoverflow.com/questions/4217037/catch-ctrl-c-in-c
-    interrupted = 0;
+    interrupted = 1;
 }
 
 
