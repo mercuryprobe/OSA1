aed99452faec1bbe5bcb0a8cd15001a601b822a8
Aryaman <someoneg4@gmail.com>
2022-10-30 23:32:34 +0530

    rfcDate (ez dub)

--- date_.c
+++ date_.c
@@ -17,12 +17,12 @@ void date_(char cmd[512][512], int flag1, int flag2, int posn, int last) {
 
     //flag detection
     int u = 0;
-    int d = 0;
+    int r = 0;
     if (flag1!=-1) {
         if (cmd[flag1][1]=='u' || cmd[flag1][1]=='U') {
             u = 1;
         }
-        if ((cmd[flag1][1]=='d' || cmd[flag1][1]=='D')) {
+        if ((cmd[flag1][1]=='r' || cmd[flag1][1]=='R')) {
             d = 1;
         }
     }
@@ -30,41 +30,31 @@ void date_(char cmd[512][512], int flag1, int flag2, int posn, int last) {
         if (cmd[flag2][1]=='u' || cmd[flag2][1]=='U') {
             u = 1;
         }
-        if ((cmd[flag2][1]=='d' || cmd[flag2][1]=='D')) {
-            d = 1;
+        if ((cmd[flag2][1]=='r' || cmd[flag2][1]=='R')) {
+            r = 1;
         }
     }
 
     cmd[last-1][strcspn(cmd[last-1], "\n")]=0;
-    if (d==0) {
-        time_t t;
-        time(&t);
-        
-        if (u==0) {
-            //default output
-            printf(ctime(&t));
-        } else {
-            //GMT time
-            struct tm *gmtTm = gmtime(&t);
-            time_t gmt = mktime(gmtTm);
-            printf(ctime(&gmt));
-        }
-    } else {
-        if (last==posn+3) {
-            struct tm *inpTime;
-            const char format[] = "%d %m %y";
-            char input[512] = "";
-            strcat(input, cmd[posn]);
-            strcat(input, " ");
-            strcat(input, cmd[posn+1]);
-            strcat(input, " ");
-            strcat(input, cmd[posn+2]);
 
-            // printf("%s\n", input);
-            // strptime(input, format, inpTime);
-            time_t time = mktime(inpTime);
-            printf(ctime(&time));
-        }
+    time_t t;
+    time(&t);
+    
+    if (u==0 && r==0) {
+        //default output
+        printf(ctime(&t));
+    } else if (u==1){
+        //GMT time
+        struct tm *gmtTm = gmtime(&t);
+        time_t gmt = mktime(gmtTm);
+        printf(ctime(&gmt));
+
+    } else {
+        struct tm *inpTime = localtime(&t);
+        const char format[] = "%a, %d %b %Y %H:%M:%S %z";
+        char rfcDate[64];
+        strftime(rfcDate, sizeof(rfcDate), format, inpTime);
+        puts(rfcDate);
     }
 }
 
