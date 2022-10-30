f9195fa5d90f6ac23411c9b3788e61cd3b69df75
Aryaman <someoneg4@gmail.com>
2022-10-31 04:09:47 +0530

    dbg

--- shell.c
+++ shell.c
@@ -163,7 +163,7 @@ static volatile sig_atomic_t active = 1;
 static int catC = 0;
 static void interrupter(int x) {
     //reference: https://stackoverflow.com/questions/4217037/catch-ctrl-c-in-c
-    if (catC==0) {active = 0; catC = 1;}
+    if (catC==0) {active = 0; catC = 0;}
 }
 
 
