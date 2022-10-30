c69ea501c2664d43af502c8b1b697c7cf8feea7d
Aryaman <someoneg4@gmail.com>
2022-10-30 17:31:55 +0530

    cleanup

--- cat_.c
+++ cat_.c
@@ -97,16 +97,10 @@ void cat(char cmd[512][512], int flag1, int flag2, int posn, int last) {
 int main(int argc, char *argv[]) {
     //tokenise input
     struct splitStruc tokens = tokenise(argv[0]);
-    
-    //get flag info
-    // char splitString[512][512];
-    // for (int i = 0; i<argc; i++) {
-    //     strcpy(splitString[i], argv[i]);
-    // }
-
     tokens.splitString[0][strcspn(tokens.splitString[0], "\n")]=0;
-    struct flagStruc floogs = flagger(tokens.splitString, argc+1);
 
+    //flag detection
+    struct flagStruc floogs = flagger(tokens.splitString, argc+1);
     int flag1 = floogs.flag1;
     int flag2 = floogs.flag2;
     int flag1Taken = floogs.flag1Taken;
